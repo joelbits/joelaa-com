9f596c9bc3db6a9fb367cac07b5123bf934db4c6
Joak Dev <joak.dev@gmail.com>
2019-01-11 17:56:41 +0100

    Menu fixes and Blog Header fixes

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+import BlogLayout from '../components/bloglayout'
 import SEO from '../components/seo'
 
 const Blog = () => (
-  <Layout>
+  <BlogLayout>
     <SEO title="Blog" />
     <h1>Hi from the Blog page</h1>
     <p>Welcome to Blog page.</p>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  </BlogLayout>
 )
 
 export default Blog
