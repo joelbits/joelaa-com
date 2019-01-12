4ad8da29b709c71ab05437070309ec95c2a1ed9e
Joak Dev <joak.dev@gmail.com>
2019-01-12 11:41:14 +0100

    Mobile fixes to Skills section

--- src/components/layout.js
+++ src/components/layout.js
@@ -57,8 +57,7 @@ const Layout = ({ children, css }) => (
           style={{
             margin: `0 auto`,
             maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            padding: `0.25em`,
           }}
         >
           {children}
