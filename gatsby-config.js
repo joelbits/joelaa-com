459f0421202983074d913bccc2b63b48c3f0162f
Joel AA <joak.dev@gmail.com>
2019-06-30 14:15:49 +0200

    config fix, options not avaiable in CI

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,8 +40,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID || options.spaceId,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || options.accessToken,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       }
     },
     {
