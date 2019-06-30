a3df7835c829c6dcc6b9b6a99317049a4c45d906
Joel AA <joak.dev@gmail.com>
2019-06-30 12:21:28 +0200

    View Site Code footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,6 +10,7 @@ const Footer = ({ data }) => {
             {email}
         </a>
         <a href={`//github.com/${github}`} className={styles.githubLink} target="_blank" rel="noopener noreferrer" title="View Github profile"><FaGithub className={styles.githubLogo} /></a>
+        <a href={`//github.com/${github}/joelaa-cms`} className={styles.githubProjectLink} target="_blank" rel="noopener noreferrer" title="View site code">View site code</a>
         <p>
           © {new Date().getFullYear()} {name} @ {(typeof window !== 'undefined') && new URL(window.location.href).hostname}
         </p>
